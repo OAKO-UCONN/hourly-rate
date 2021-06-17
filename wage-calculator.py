385763e24a6e6778281974843f47941e3db5a95d
OAKO-UCONN <63368794+OAKO-UCONN@users.noreply.github.com>
2021-06-16 20:33:57 -0400

    Error handling try: and except:

--- wage-calculator.py
+++ wage-calculator.py
@@ -2,8 +2,10 @@ def payment_computation(r, hours):
     if hours > 40:
         return (hours-40)*(1.5*r) + r*40
     return r*hours
-
-r = eval(input('Please enter hourly rate'))
-hours = eval(input('Please enter number of hours'))
-result = payment_computation(r,hours)
-print(result)
+try:
+    r = eval(input('Please enter hourly rate'))
+    hours = eval(input('Please enter number of hours'))
+    result = payment_computation(r,hours)
+    print(result)
+except:
+    
