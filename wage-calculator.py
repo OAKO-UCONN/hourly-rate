cbf4da56eaf942cba3a24ae15dcb1effbdb7e77a
OAKO-UCONN <63368794+OAKO-UCONN@users.noreply.github.com>
2021-06-16 20:40:24 -0400

    Update wage-calculator.py

--- wage-calculator.py
+++ wage-calculator.py
@@ -2,10 +2,15 @@ def payment_computation(r, hours):
     if hours > 40:
         return (hours-40)*(1.5*r) + r*40
     return r*hours
-try:
-    r = eval(input('Please enter hourly rate'))
-    hours = eval(input('Please enter number of hours'))
-    result = payment_computation(r,hours)
-    print(result)
-except:
-    
+
+indicator = 0
+while indicator == 0:
+    try:
+        r = eval(input('Please enter hourly rate.'))
+        hours = eval(input('Please enter number of hours.'))
+        result = payment_computation(r,hours)
+        print(result)
+        indicator = 1
+    except:
+        print('Please enter a number next time.')
+        
